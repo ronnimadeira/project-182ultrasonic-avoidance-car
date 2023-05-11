5df065d1e056f4ef60a466b194420be41f5a4d8c
ronnimadeira <115476262+ronnimadeira@users.noreply.github.com>
2023-05-11 16:09:31 -0300

    Atualizar 'main.blocks, main.ts'

--- main.ts
+++ main.ts
@@ -1,31 +1,33 @@
 function car_back () {
-    mecanumRobotV2.Motor(LR.Upper_left, MD.Back, 50)
-    mecanumRobotV2.Motor(LR.Lower_left, MD.Back, 50)
-    mecanumRobotV2.Motor(LR.Upper_right, MD.Back, 50)
-    mecanumRobotV2.Motor(LR.Lower_right, MD.Back, 50)
+    mecanumRobotV2.Motor(LR.Upper_left, MD.Back, vel)
+    mecanumRobotV2.Motor(LR.Lower_left, MD.Back, vel)
+    mecanumRobotV2.Motor(LR.Upper_right, MD.Back, vel)
+    mecanumRobotV2.Motor(LR.Lower_right, MD.Back, vel)
 }
 function car_left () {
-    mecanumRobotV2.Motor(LR.Upper_left, MD.Back, 50)
-    mecanumRobotV2.Motor(LR.Lower_left, MD.Back, 50)
-    mecanumRobotV2.Motor(LR.Upper_right, MD.Forward, 50)
-    mecanumRobotV2.Motor(LR.Lower_right, MD.Forward, 50)
+    mecanumRobotV2.Motor(LR.Upper_left, MD.Back, vel)
+    mecanumRobotV2.Motor(LR.Lower_left, MD.Back, vel)
+    mecanumRobotV2.Motor(LR.Upper_right, MD.Forward, vel)
+    mecanumRobotV2.Motor(LR.Lower_right, MD.Forward, vel)
 }
 function car_forward () {
-    mecanumRobotV2.Motor(LR.Upper_left, MD.Forward, 50)
-    mecanumRobotV2.Motor(LR.Lower_left, MD.Forward, 50)
-    mecanumRobotV2.Motor(LR.Upper_right, MD.Forward, 50)
-    mecanumRobotV2.Motor(LR.Lower_right, MD.Forward, 50)
+    mecanumRobotV2.Motor(LR.Upper_left, MD.Forward, vel)
+    mecanumRobotV2.Motor(LR.Lower_left, MD.Forward, vel)
+    mecanumRobotV2.Motor(LR.Upper_right, MD.Forward, vel)
+    mecanumRobotV2.Motor(LR.Lower_right, MD.Forward, vel)
 }
 function car_right () {
-    mecanumRobotV2.Motor(LR.Upper_left, MD.Forward, 50)
-    mecanumRobotV2.Motor(LR.Lower_left, MD.Forward, 50)
-    mecanumRobotV2.Motor(LR.Upper_right, MD.Back, 50)
-    mecanumRobotV2.Motor(LR.Lower_right, MD.Back, 50)
+    mecanumRobotV2.Motor(LR.Upper_left, MD.Forward, vel)
+    mecanumRobotV2.Motor(LR.Lower_left, MD.Forward, vel)
+    mecanumRobotV2.Motor(LR.Upper_right, MD.Back, vel)
+    mecanumRobotV2.Motor(LR.Lower_right, MD.Back, vel)
 }
 let diatance_r = 0
 let distance_l = 0
 let distance = 0
+let vel = 0
 basic.showIcon(IconNames.Happy)
+vel = 30
 basic.forever(function () {
     distance = mecanumRobotV2.ultra()
     if (distance < 20) {
